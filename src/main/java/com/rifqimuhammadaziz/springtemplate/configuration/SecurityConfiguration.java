0ec206029afe2ef8c4aaf01a812f33c9d19eed11
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-10-25 14:23:49 +0700

    add security configuration

---
+++ src/main/java/com/rifqimuhammadaziz/springtemplate/configuration/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.rifqimuhammadaziz.springtemplate.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration {
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests((authorize) -> authorize
+                        .antMatchers(HttpMethod.GET, "/api/v1/**").permitAll() // controller
+                        .antMatchers("/v2/api-docs/**").permitAll() // swagger
+                        .antMatchers("/swagger-ui/**").permitAll() // swagger
+                        .antMatchers("/swagger-resources/**").permitAll() // swagger
+                        .antMatchers("/swagger-ui.html").permitAll() // swagger
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
--- src/main/java/com/rifqimuhammadaziz/springtemplate/service/implementation/PostServiceImpl.java
+++ src/main/java/com/rifqimuhammadaziz/springtemplate/service/implementation/PostServiceImpl.java
@@ -1,35 +1,45 @@
 package com.rifqimuhammadaziz.springtemplate.service.implementation;
 
 import com.rifqimuhammadaziz.springtemplate.model.entity.Post;
+import com.rifqimuhammadaziz.springtemplate.repository.PostRepository;
 import com.rifqimuhammadaziz.springtemplate.service.contract.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
     @Override
     public Post save(Post post) {
-        return null;
+        return postRepository.save(post);
     }
 
     @Override
     public List<Post> findAll() {
-        return null;
+        return postRepository.findAll();
     }
 
     @Override
     public Post findById(Long id) {
-        return null;
+        return postRepository.findById(id).get();
     }
 
     @Override
     public Post update(Post post, Long id) {
-        return null;
+        Post currentPost = postRepository.findById(id).get();
+        currentPost.setTitle(post.getTitle());
+        currentPost.setCategory(post.getCategory());
+        currentPost.setBody(post.getBody());
+        return postRepository.save(currentPost);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        postRepository.deleteById(id);
     }
 }
